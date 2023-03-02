1246b55541680aedf474b063f11a2dcdcd4cc716
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-03-02 20:35:25 +0200

    add readline to work with console inputs

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
-'use strict';
+"use strict";
 
 const { dir } = require("console");
 const fs = require("fs");
-const { basename, dirname } = require("path");
-const path = require('path');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
+const path = require("path");
+const readline = require("readline");
 
 class FileSystem {
-    constructor(){
-        this.user = '';
-        this.rootDir =  path.join(__dirname + '/fs')
+  constructor() {
+    this.user = "";
+    this.rootDir = path.join(__dirname + "/fs");
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+  }
+
+  async start() {
+    let username = await new Promise((resolve) => {
+      this.rl.question("Username: ", resolve);
+    });
+    if (username && username.length > 0) {
+      this.user = username;
     }
+  }
 }
 
-const fileSys = new FileSystem()
-console.log(fileSys.rootDir)
+(async () => {
+  const fileSys = new FileSystem();
+  await fileSys.start();
+  console.log(fileSys.user)
+  // console.log(fileSys.rootDir)
+})();
