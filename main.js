e401212fddb700353a7563b9cb8e19c6a81343ae
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-03-02 20:20:51 +0200

    add some directories that will emulate drives

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log("hello world")
+const { dir } = require("console");
+const fs = require("fs");
+const { basename, dirname } = require("path");
+const path = require('path');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+
+class FileSystem {
+    constructor(){
+        this.user = '';
+        this.rootDir =  path.join(__dirname + '/fs')
+    }
+}
+
+const fileSys = new FileSystem()
+console.log(fileSys.rootDir)
