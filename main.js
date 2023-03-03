6ad1bb2236f30997ca98154ce78078c8c3b2a473
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-03-03 15:30:52 +0200

    add readline listener and also setPromt

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 "use strict";
 
-const { dir } = require("console");
 const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
 
 class FileSystem {
   constructor() {
     this.user = "";
     this.rootDir = path.join(__dirname + "/fs");
+    this.currentPath = path.join(__dirname + "/fs");
+    this.prompt = "> ";
     this.rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
@@ -22,12 +26,34 @@ class FileSystem {
     if (username && username.length > 0) {
       this.user = username;
     }
+    // Set prompt
+    this.prompt = this.user + "@" + '/' + "> "
+    this.rl.setPrompt(this.prompt)
+
+    // Start listening for input events
+    this.processLines();
+
+    this.rl.prompt()
+  }
+
+  processLines() {
+    // Listen for input events
+    this.rl.on("line", (input) => {
+      // Handle input here
+      console.log(`Received input: ${input}`);
+
+      // Print user and prompt
+      process.stdout.write(`${this.user}@${this.prompt}`);
+
+      // Re-display input field
+      this.rl.prompt();
+    });
+
   }
 }
 
 (async () => {
   const fileSys = new FileSystem();
   await fileSys.start();
-  console.log(fileSys.user)
-  // console.log(fileSys.rootDir)
+
 })();
