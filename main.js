7979002c93bab1136f78a0db0ec977525a2cdcc3
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-03-04 20:48:07 +0200

    made pwd command

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ process.stdin.setRawMode(true);
 class FileSystem {
   constructor() {
     this.user = "";
-    this.rootDir = path.join(__dirname + "/fs");
-    this.currentPath = path.join(__dirname + "/fs");
+    this.rootDir = path.join(__dirname + "/fs/");
+    this.currentPath = path.join(__dirname + "/fs/");
     this.prompt = "> ";
     this.rl = readline.createInterface({
       input: process.stdin,
@@ -27,33 +27,51 @@ class FileSystem {
       this.user = username;
     }
     // Set prompt
-    this.prompt = this.user + "@" + '/' + "> "
-    this.rl.setPrompt(this.prompt)
+    this.prompt = this.user + "@" + "/" + "> ";
+    this.rl.setPrompt(this.prompt);
 
     // Start listening for input events
     this.processLines();
 
-    this.rl.prompt()
+    this.rl.prompt();
   }
 
   processLines() {
     // Listen for input events
     this.rl.on("line", (input) => {
       // Handle input here
-      console.log(`Received input: ${input}`);
+      let strArr = input.split(" ");
+      if (strArr[0].length < 1) {
+        console.log("if works");
+        this.rl.prompt();
+        return;
+      }
+      // pwd, ls, cd, mkdir, vi, rm
+      let command = strArr.shift().toLocaleLowerCase();
+      if (command == "pwd") {
+        console.log("/" + path.relative(this.rootDir, this.currentPath));
+        // console.log("Current location: ", this.currentPath)
+      }
+      if (command == "cd") {
+        console.log("CD command");
+        // return;
+      }
+      if (command == "ls") {
+        console.log("LS command");
+        // return;
+      }
+      //   console.log("command: ", command);
 
-      // Print user and prompt
-      process.stdout.write(`${this.user}@${this.prompt}`);
+      //   console.log(strArr);
+      //   console.log(`Received input: ${input}`);
 
       // Re-display input field
       this.rl.prompt();
     });
-
   }
 }
 
 (async () => {
   const fileSys = new FileSystem();
   await fileSys.start();
-
 })();
